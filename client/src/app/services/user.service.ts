a0cf90c573d01b15193274ba061675379b6392d3
montalvoCode <montalvocode1997@hotmail.com>
2018-03-18 19:10:20 -0500

    Servicios de subida de avatar

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -85,6 +85,6 @@ export class UserService {
         let headers = new HttpHeaders().set('Content-Type', 'application/json')
             .set('Authorization', this.getToken());
 
-        return this._http.put(this.user + 'update-user/' + user._id, params, { headers: headers });
+        return this._http.put(this.url + 'update-user/' + user._id, params, { headers: headers });
     }
 }   
